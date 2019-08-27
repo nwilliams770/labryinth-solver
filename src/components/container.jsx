11ce457c2c640bac3991751225b0db24e6c7986b
Nicholas Williams <nwilliams770@gmail.com>
2019-08-27 15:58:50 -0700

    first commit / boilerplate

---
+++ src/components/container.jsx
@@ -0,0 +1,20 @@
+import '../stylesheets/app.scss';
+
+const Navbar = () => {
+    return (
+        <div id="nav">
+            <h3>I'm the navbar</h3>
+        </div>
+    )
+}
+
+function Container () {
+    return (
+        <div id="app">
+            <h1>Hello world, I'm your app</h1>
+        </div>
+        
+    )
+}
+
+export default Container;
