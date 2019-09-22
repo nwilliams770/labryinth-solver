ba066f8943609526ec30ae19dc32fb0912a1a306
Nicholas Williams <nwilliams770@gmail.com>
2019-09-22 15:48:18 -0700

    add react-sprite-animator fix

---
+++ src/react-sprite-animator.d.ts
@@ -0,0 +1,9 @@
+declare module 'react-sprite-animator' {
+    class ReactSpriteAnimator {
+        constructor(opts?: any)
+
+        base(opts?: any): any
+    }
+
+    export default ReactSpriteAnimator;
+}
