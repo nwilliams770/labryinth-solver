02000a33d6dbe514ea9ee4c042abefe7469234d5
Nicholas Williams <nwilliams770@gmail.com>
2019-10-21 10:09:11 -0700

    sorcerer sprite firing on algo selection

--- src/components/sprites/sorcerer.jsx
+++ src/components/sprites/sorcerer.jsx
@@ -1,27 +1,46 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect, useCallback } from 'react';
 import SpriteAnimator from 'react-sprite-animator';
 import sorcererSpriteSheet from '../../images/sorcerer-sprite.png'; // weird to do but per the docs I guess https://create-react-app.dev/docs/adding-images-fonts-and-files
+import MazeStore from '../../stores/maze-store';
 
 const Sorcerer = (props) => {
-    const [isClicked, setClicked] = useState(false);
+    const [shouldAnimate, setAnimate] = useState(true);
     const spriteEl = useRef(null);
 
+    const handleSpriteEvent = useCallback((event) => {
+        // Don't think this is best practice here but we need a way to toggle shouldAnimate
+        // from true back to false on each spriteEvent
+        // Kind of a constraint of using this external software
+        // In hindsight, it would've been better to initiate sprite with a function call instead of a bool value
+        setAnimate(true);
+        setTimeout(() => {
+            setAnimate(false);
+        }, 1500);
+    });
+
+    useEffect(() => {
+        MazeStore.addSpriteEventListener('sorcerer', handleSpriteEvent);
+
+        return () => {
+            MazeStore.removeSpriteEventListener('sorcerer', handleSpriteEvent);
+        }
+    }, [handleSpriteEvent]); 
+
     return (
-        <div id="sorcerer" class="sprite" onClick={() => setClicked(!isClicked)}>
+        <div id="sorcerer" className="sprite">
             <SpriteAnimator
                 ref={spriteEl}
                 width={210}
                 height={190}
                 sprite={sorcererSpriteSheet}
                 direction="vertical"
-                shouldAnimate={isClicked}
+                shouldAnimate={shouldAnimate}
                 fps={10}
                 startFrame={0}
                 stopLastFrame={true}
-                reset={!isClicked}
-                // frameCount={9}
                 frameCount={15}
                 wrapAfter={1}
+                reset={!shouldAnimate}
             />
     </div>
     )
