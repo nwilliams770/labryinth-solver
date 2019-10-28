92057db5f710cc51277ceb93097dcc42754d5405
Nicholas Williams <nwilliams770@gmail.com>
2019-10-28 16:34:42 -0700

    add app

---
+++ src/components/app.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Sorcerer from './sprites/sorcerer';
+import Maze from './maze/maze';
+import SelectorModules from './selector/selector_modules';
+
+
+const App = () => {
+    return (
+        <div id="app">
+            <SelectorModules />
+            <Sorcerer />
+            <Maze />
+        </div>
+        
+    )
+}
+
+export default App;
