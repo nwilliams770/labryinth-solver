96079ba00178e7dfdfce1de78fc5279731a690af
Nicholas Williams <nwilliams770@gmail.com>
2019-12-01 20:13:42 -0800

    fix initial animation bug

--- src/components/sprites/sorcerer.jsx
+++ src/components/sprites/sorcerer.jsx
@@ -20,6 +20,10 @@ const Sorcerer = (props) => {
 
     useEffect(() => {
         MazeStore.addSpriteEventListener('sorcerer', handleSpriteEvent);
+        // Set to false for future animation triggers
+        setTimeout(() => {
+            setAnimate(false);
+        }, 1500);
 
         return () => {
             MazeStore.removeSpriteEventListener('sorcerer', handleSpriteEvent);
