198ba9eb45f971cef15f5e571139e314514135bf
Nicholas Williams <nwilliams770@gmail.com>
2020-01-18 18:04:53 -0800

    style footer and drawer

--- src/components/footer/about.jsx
+++ src/components/footer/about.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
 import SlidingPanel from 'react-sliding-side-panel';
 import trigger from '../../images/about-trigger.gif';
+import header from '../../images/alaska-header.png';
+import subHeader from '../../images/sub-header.png';
+import arrow from '../../images/selector-arrow.png';
+
 
 const About = () => {
     const [openPanel, setOpenPanel] = useState(false);
@@ -14,10 +18,67 @@ const About = () => {
                 type={'left'}
                 isOpen={openPanel}
                 size={35} >
-                    <div id="drawer">
-                        <h3>Hello!</h3>
-                        <button onClick={() => (setOpenPanel(false))}>Hiya!</button>
+                    <div id="drawer-header">
+                        <button onClick={() => (setOpenPanel(false))}>
+                            <img src={arrow} alt="Close sliding drawer"/>
+                        </button>
+                        <img id="drawer-title" src={header} alt="Alaska:" />
+                        <img id="drawer-sub-title" src={subHeader} alt="The Trial of the Maze Wizard" />
+                        <div class="intro">
+                            <p class="copy">
+                                Alaska: The Trial of the Maze Wizard is a visual exploration of various 
+                                graph traversing algorithms applied to maze representations. <br/>
+                                Built with React + HTML5 Canvas.
+                            </p>
+                        </div>
                     </div>
+                    <div id="drawer-content">
+                        <div class="item">
+                            <div class="title">
+                                <h4 class="hdr">Trémaux || Depth-First Search (DFS)</h4>
+                            </div>
+                            <div class="body">
+                                <ul>
+                                    <li class="point">Loren Ipsum</li>
+                                    <li class="point">Loren Ipsum</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="title">
+                                <h4 class="hdr">Wall Follower || Right-Hand Rule</h4>
+                            </div>
+                            <div class="body">
+                                <ul>
+                                    <li class="point">Loren Ipsum</li>
+                                    <li class="point">Loren Ipsum</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="title">
+                                <h4 class="hdr">Breadth-First Search (BFS)</h4>
+                            </div>
+                            <div class="body">
+                                <ul>
+                                    <li class="point">Loren Ipsum</li>
+                                    <li class="point">Loren Ipsum</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="title">
+                                <h4 class="hdr">A<sup>*</sup></h4>
+                            </div>
+                            <div class="body">
+                                <ul>
+                                    <li class="point">Loren Ipsum</li>
+                                    <li class="point">Loren Ipsum</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <a href="https://github.com/nwilliams770/labryinth-solver">See the repo</a>
             </SlidingPanel>
         </div>
     )
--- src/components/footer/contact.jsx
+++ src/components/footer/contact.jsx
@@ -6,12 +6,12 @@ import githubLogo from '../../images/github-logo.png';
 const Contact = () => {
     return (
         <div id="contact">
-            <p>Created by <a href="WEBSITE_LINK ">Nicholas Williams</a></p>
+            <p>Created by <a href="https://www.nico-williams.com/ ">Nicholas Williams</a></p>
             <div id="icons">
-                <a id="github" href="GITHUB LINK" target="_blank" rel="noopener noreferrer">
+                <a id="github" href="https://github.com/nwilliams770/" target="_blank" rel="noopener noreferrer">
                     <img src={githubLogo} alt="an 8-bit version of the Github logo"/>
                 </a>
-                <a id="linkedin" href="LINKEDIN LINK" target="_blank" rel="noopener noreferrer">
+                <a id="linkedin" href="https://www.linkedin.com/in/nwilliams770/" target="_blank" rel="noopener noreferrer">
                     <img src={linkedinLogo} alt="an 8-bit version of the LinkedIn logo"/>
                 </a>
             </div>
