71cb648e828b3e87bc10ae6835d99d9b4790402f
Nicholas Williams <nwilliams770@gmail.com>
2020-02-05 13:19:33 -0800

    remove inline image bg and add to sass instead

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -3,15 +3,10 @@ import Ship from './sprites/ship';
 import header from '../images/alaska-header.png';
 import subHeader from '../images/sub-header.png';
 import copy from '../images/intro-copy.png';
-import bg from '../images/intro-bg.jpg';
-
-
-
 
 const Intro = () => {
     return (
         <div id="intro">
-            <img id="intro-bg" src={bg} alt=""/>
             <img id="hdr" src={header} alt="Alaska" />
             <img id="sub-hdr" src={subHeader} alt="The Trial of the Maze Wizard" />
             <img id="click-text" className="animate-flicker" src={copy} alt="Click anywhere to start." />
